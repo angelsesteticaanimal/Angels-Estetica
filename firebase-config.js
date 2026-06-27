56f0be7d6e0f2eeaeb9e10771d58a18089b57bc8
angelsesteticaanimal <geisonpixpicpay@gmail.com>
2026-06-27 06:58:07 -0300

    Update Firebase config and enable Firebase
    Updated Firebase configuration with new project details and enabled Firebase.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,13 @@
-// Preparado para Firebase na próxima etapa.
-window.firebaseEnabled = false;
+// Angels Estética Animal - Firebase Config
+
+window.firebaseEnabled = true;
+
 window.firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyBod60eSEo-zfVsa4XD_ZzWVzKpJlsZVcM",
+  authDomain: "angels-agenda.firebaseapp.com",
+  projectId: "angels-agenda",
+  storageBucket: "angels-agenda.firebasestorage.app",
+  messagingSenderId: "796463233277",
+  appId: "1:796463233277:web:471e0796be00b489812694",
+  measurementId: "G-Y2M68MDTGR"
 };
