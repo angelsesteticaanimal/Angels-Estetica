5fbcb49964fe49bc4c58f99fdc6659a53130540f
angelsesteticaanimal <geisonpixpicpay@gmail.com>
2026-06-27 07:10:56 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,10 @@
-// Angels Estética Animal - Firebase Config
-
-window.firebaseEnabled = true;
-
+// Preparado para Firebase na próxima etapa.
+window.firebaseEnabled = false;
 window.firebaseConfig = {
-  apiKey: "AIzaSyBod60eSEo-zfVsa4XD_ZzWVzKpJlsZVcM",
-  authDomain: "angels-agenda.firebaseapp.com",
-  projectId: "angels-agenda",
-  storageBucket: "angels-agenda.firebasestorage.app",
-  messagingSenderId: "796463233277",
-  appId: "1:796463233277:web:471e0796be00b489812694",
-  measurementId: "G-Y2M68MDTGR"
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
 };
